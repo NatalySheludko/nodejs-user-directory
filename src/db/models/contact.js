b8aa78b346ee21379f9be298861c71f40c277e55
nataliia <sheludkonataliyavaleriivna@gmail.com>
2024-07-13 14:18:51 +0300

    create contactsCollection models

---
+++ src/db/models/contact.js
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose';
+
+const contactsSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      optional: true,
+    },
+    isFavourite: {
+      type: Boolean,
+      default: false,
+    },
+    contactType: {
+      type: String,
+      enum: ['work', 'home', 'personal'],
+      required: true,
+      default: 'personal',
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const ContactsCollection = mongoose.model('contacts', contactsSchema);
