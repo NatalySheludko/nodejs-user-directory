9cfcb48609610c7c736f95f4003a41fa3c0957b2
Nataliia Sheludko <sheludkonataliyavaleriivna@gmail.com>
2024-07-17 22:29:16 +0300

    replace routers, import http-errors funcs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { initMongoConnection } from './db/initMongoConnection.js';
 import { setupServer } from './server.js';
 
+
 const bootstrap = async () => {
   try {
     await initMongoConnection();
