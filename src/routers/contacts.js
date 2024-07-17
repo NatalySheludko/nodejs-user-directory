06ca3828c2d07125b4f6689091ecee30ada87675
Nataliia Sheludko <sheludkonataliyavaleriivna@gmail.com>
2024-07-17 22:31:31 +0300

    create routers with ctrlWrap

---
+++ src/routers/contacts.js
@@ -0,0 +1,21 @@
+import { Router } from 'express';
+import {
+  getContactsController,
+	getContactByIdController,
+	createContactController,
+	deleteContactController,
+	upsertContactController,
+	patchContactController,
+} from '../controllers/contacts.js';
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+
+const router = Router();
+
+router.get('/contacts', ctrlWrapper(getContactsController));
+router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
+router.post('/contacts', ctrlWrapper(createContactController));
+router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController));
+router.put('/contacts/:contactId', ctrlWrapper(upsertContactController));
+router.patch('/contacts/:contactId', ctrlWrapper(patchContactController));
+
+export default router;
