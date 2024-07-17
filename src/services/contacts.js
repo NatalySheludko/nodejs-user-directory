4f385e00a05d4309162e3eda81d56c4eeff6635d
Nataliia Sheludko <sheludkonataliyavaleriivna@gmail.com>
2024-07-17 22:32:20 +0300

    add all http requests services

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,5 +1,6 @@
 import { ContactsCollection } from '../db/models/contact.js';
 
+
 export const getAllContacts = async () => {
   const contacts = await ContactsCollection.find();
   return contacts;
@@ -8,4 +9,36 @@ export const getAllContacts = async () => {
 export const getContactById = async (contactId) => {
   const contact = await ContactsCollection.findById(contactId);
   return contact;
+};
+
+export const createContact = async (payload) => {
+  const contact = await ContactsCollection.create(payload);
+  return contact;
+};
+
+export const deleteContact = async (contactId) => {
+  const contact = await ContactsCollection.findOneAndDelete({
+    _id: contactId,
+	});
+
+  return contact;
+};
+
+export const updateContact = async (contactId, payload, options = {}) => {
+  const rawResult = await ContactsCollection.findOneAndUpdate(
+    { _id: contactId },
+    payload,
+    {
+      new: true,
+      includeResultMetadata: true,
+      ...options,
+    },
+	);
+
+	if (!rawResult || !rawResult.value) return null;
+	
+  return {
+    contact: rawResult.value,
+    isNew: Boolean(rawResult?.lastErrorObject?.upserted),
+  };
 };
