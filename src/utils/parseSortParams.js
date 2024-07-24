9b521b5461c5c4fc004560195e492fb2cfe5d1eb
Nataliia Sheludko <sheludkonataliyavaleriivna@gmail.com>
2024-07-24 23:40:58 +0300

    add parseSortParams funcs

---
+++ src/utils/parseSortParams.js
@@ -0,0 +1,36 @@
+import { SORT_ORDER } from '../constants/index.js';
+
+const parseSortOrder = (sortOrder) => {
+  const isKnownOrder = [SORT_ORDER.ASC, SORT_ORDER.DESC].includes(sortOrder);
+  if (isKnownOrder) return sortOrder;
+  return SORT_ORDER.ASC;
+};
+
+const parseSortBy = (sortBy) => {
+  const keysOfStudent = [
+    '_id',
+    'name',
+    'phoneNumber',
+    'email',
+    'isFavourite',
+    'contactType',
+  ];
+
+  if (keysOfStudent.includes(sortBy)) {
+    return sortBy;
+  }
+
+  return '_id';
+};
+
+export const parseSortParams = (query) => {
+  const { sortOrder, sortBy } = query;
+
+  const parsedSortOrder = parseSortOrder(sortOrder);
+  const parsedSortBy = parseSortBy(sortBy);
+
+  return {
+    sortOrder: parsedSortOrder,
+    sortBy: parsedSortBy,
+  };
+};
