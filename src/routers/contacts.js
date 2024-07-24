50a5bf65d10d742746af1f9a46e2af10762d7fb9
Nataliia Sheludko <sheludkonataliyavaleriivna@gmail.com>
2024-07-24 23:38:11 +0300

    update with validations

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -8,22 +8,44 @@ import {
   patchContactController,
 } from '../controllers/contacts.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { validateBody } from '../middlewares/validateBody.js';
+import {
+  createContactSchema,
+  updateContactSchema,
+} from '../validation/contacts.js';
+import { isValidId } from '../middlewares/isValidId.js';
 
 const router = express.Router();
 const jsonParser = express.json();
 
 router.get('/contacts', ctrlWrapper(getContactsController));
-router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
-router.post('/contacts', jsonParser, ctrlWrapper(createContactController));
-router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController));
+router.get(
+  '/contacts/:contactId',
+  isValidId,
+  ctrlWrapper(getContactByIdController),
+);
+router.post(
+  '/contacts',
+  validateBody(createContactSchema),
+  jsonParser,
+  ctrlWrapper(createContactController),
+);
+router.delete(
+  '/contacts/:contactId',
+  isValidId,
+  ctrlWrapper(deleteContactController),
+);
 router.put(
   '/contacts/:contactId',
+  isValidId,
   jsonParser,
   ctrlWrapper(upsertContactController),
 );
 router.patch(
   '/contacts/:contactId',
+  isValidId,
   jsonParser,
+  validateBody(updateContactSchema),
   ctrlWrapper(patchContactController),
 );
 
