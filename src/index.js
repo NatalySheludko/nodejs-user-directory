03ccfde1922c63435fe04509901b4a84771fee21
Nataliia Sheludko <sheludkonataliyavaleriivna@gmail.com>
2024-08-10 16:23:04 +0300

    add auto create not exist files func

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import { initMongoConnection } from './db/initMongoConnection.js';
 import { setupServer } from './server.js';
-
+import { createDirIfNotExists } from './utils/createDirIfNotExists.js';
+import { TMP_UPLOAD_DIR, UPLOAD_AVATAR_DIR } from './constants/index.js';
 
 const bootstrap = async () => {
   try {
     await initMongoConnection();
+    await createDirIfNotExists(TMP_UPLOAD_DIR);
+    await createDirIfNotExists(UPLOAD_AVATAR_DIR);
     setupServer();
   } catch (error) {
     console.error(error);
