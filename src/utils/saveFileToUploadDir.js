b997a786fa48c6cdbcda79fa1831ab0240443a9d
Nataliia Sheludko <sheludkonataliyavaleriivna@gmail.com>
2024-08-10 16:24:13 +0300

    save img to dir

---
+++ src/utils/saveFileToUploadDir.js
@@ -0,0 +1,15 @@
+import path from 'node:path';
+import * as fs from 'node:fs/promises';
+import { TMP_UPLOAD_DIR, UPLOAD_AVATAR_DIR } from '../constants/index.js';
+
+export const saveFileToUploadDir = async (file) => {
+  await fs.rename(
+    path.join(TMP_UPLOAD_DIR, file.filename),
+    path.join(UPLOAD_AVATAR_DIR, file.filename),
+  );
+
+  return `${process.env.APP_DOMAIN}/uploads/${file.filename}`;
+};
+
+
+
