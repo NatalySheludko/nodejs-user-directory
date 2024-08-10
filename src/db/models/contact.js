bbe78220483239b2851ce5a58165959bf661b81b
Nataliia Sheludko <sheludkonataliyavaleriivna@gmail.com>
2024-08-10 16:25:27 +0300

    add photo field to schema

--- src/db/models/contact.js
+++ src/db/models/contact.js
@@ -29,6 +29,10 @@ const contactsSchema = new mongoose.Schema(
       type: mongoose.Schema.Types.ObjectId,
       required: true,
     },
+    photo: {
+      type: String,
+      default: null,
+    },
   },
   {
     timestamps: true,
