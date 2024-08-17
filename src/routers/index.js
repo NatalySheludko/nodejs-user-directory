0a175d7692246e403c17a9b870e9909d57c964c5
Nataliia Sheludko <sheludkonataliyavaleriivna@gmail.com>
2024-08-17 16:06:43 +0300

    add api-docs router

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import contactsRouter from './contacts.js';
 import authRouter from './auth.js';
+import { swaggerDocs } from '../middlewares/swaggerDocs.js';
 
 const router = express.Router();
 
 router.use('/contacts', contactsRouter);
 router.use('/auth', authRouter);
+router.use('/api-docs', swaggerDocs());
 
 export default router;
